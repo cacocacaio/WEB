8d77effe1e7c626191e730c1e6898da29fa7eed4
alunos <cacocacaio>
2016-08-19 10:17:06 -0300

    eu sofria agr eu sou fria

---
+++ 3bim/Atividade1/aval_02bim/cliente-edit.php
@@ -0,0 +1,78 @@
+<?php
+	require 'init.php';
+	//pega o ID da URL
+	$id = isset($_GET['id']) ? (int) $_GET['id'] : null;
+	//valida o ID
+	if(empty($id)){
+		
+		echo "ID para alteracao nao definido";
+		exit;
+	}
+	//busca os dados do cliente a ser editado
+	$PDO = db_connect();
+	$sql = "SELECT nomeCliente, dataCadastro, email FROM clientes WHERE idCliente= :id";
+	$stmt = $PDO->prepare($sql);
+	$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+	$stmt->execute();
+	$cliente = $stmt->fetch(PDO::FETCH_ASSOC);
+	//se o método fetch() não retornar um array, o ID não corresponde a um cliente válido
+	if(!is_array($cliente)){
+		
+		echo "Nenhum cliente encontrado";
+		exit;
+	}
+	$dataOK = dateConvert($cliente['dataCadastro']);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+	
+		<title>Cadastro de Cliente</title>
+		<meta charset="utf-8">
+		<link type="text/css" href="css/jquery-ui.css" rel="stylesheet">
+		<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.js"></script>
+		<script type="text/javascript" src="js/datepicker-pt-BR.js"></script>
+		<script type="text/javascript" src="js/meuscript.js"></script>
+	</head>
+		
+	<body>
+		<div class="conteudo">
+			<form method="post" name="formAltera" action="editCliente.php" enctype="multipart/form-data" onsubmit="return validarCliente();">
+		
+				<h1>Edição de Cliente</h1>
+			
+				<table width="100%">
+			
+					<tr>
+					
+						<th width="18%">Nome</th>
+						<td width="82%"><input type="text" name="txtNome" id="nome" value="<?php echo $cliente['nomeCliente'] ?>">
+						<span id="textoNome">Nome Invalido</span></td>
+					</tr>
+					
+					<tr>
+					
+						<th width="18%">Data de Cadastro</th>
+						<td width="82%"><input type="text" name="txtData" id="datacadastro" value="<?php echo $dataOK ?>">
+						<span id="textoDataCadastro">Data Invalida</span></td>
+					</tr>
+					
+					<tr>
+					
+						<th width="18%">Email</th>
+						<td width="82%"><input type="text" name="txtEmail" id="email" value="<?php echo $cliente['email'] ?>">
+						<span id="textoEmail">E-mail Invalido</span></td>
+					</tr>
+					
+					<tr class="entrada">
+					
+						<input type="hidden" name="id" value="<?php echo $id ?>">
+						<td><input class="HOME" type="submit" name="btnEnvia" value="Alterar"></td>
+						<td><input class="HOME" type="reset" name="btnLimpar" value="Limpar"></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ 3bim/Atividade1/aval_02bim/relatorio_cliente.php
@@ -0,0 +1,72 @@
+<div class="conteudo">
+	<?php
+
+		// inclui classes necessárias
+		spl_autoload_register(function ($class_name){
+			
+			include 'html/'.$class_name.'.class.php';
+		});
+
+		require_once 'init.php';
+		
+		$PDO = db_connect();
+		
+		$sql = "SELECT idCliente, nomeCliente, dataCadastro, email FROM clientes ORDER BY nomeCliente ASC";
+		$stmt = $PDO->prepare($sql);
+		$stmt->execute();
+		
+		
+		// *********************************************************
+		// corrigir caracteres
+		$html = new TElement('html');
+		$html->lang = 'pt-br';
+
+		$body = new TElement('body');
+		$html->add($body);
+	  
+		//instancia objeto-tabela
+		$tabela = new TTable;
+		//define algumas propriedades
+		$tabela->width = 600;
+		$tabela->align = "center";
+		$tabela->border = 1;
+		//instancia uma linha para o cabeçalho
+		$cabecalho = $tabela->addRow();
+		//define a cor de fundo
+		$cabecalho->bgcolor = '#4cde70';
+		//adiciona células
+		$cabecalho->addCell('Id');
+		$cabecalho->addCell('Nome');
+		$cabecalho->addCell('E-Mail');
+		$cabecalho->addCell('Data de Cadastro');
+
+		$i = 0;
+		$total = 0;
+		//percorre os dados
+		
+		while ($clientes = $stmt->fetch(PDO::FETCH_ASSOC)){
+			
+			//foreach($clientes as $pessoa){
+				
+				// verifica qual cor utilizará para o fundo
+				$bgcolor = ($i % 2) == 0 ? '#000000' : '#3B3B3B';
+				// adiciona uma linha para os dados
+				$linha = $tabela->addRow();
+				$linha->bgcolor = $bgcolor;
+				// adiciona as células
+				$linha->addCell($clientes['idCliente']);
+				$linha->addCell($clientes['nomeCliente']);
+				$linha->addCell($clientes['email']);
+				$x = $linha->addCell(dateConvert($clientes['dataCadastro']));
+				$x->align = 'right';
+				
+				$i++;
+			//}
+		}
+		
+		// exibe a tabela
+		//$tabela->show();
+		$body->add($tabela);
+		$html->show();
+	?>
+</div>
